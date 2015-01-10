0dd8ef6da298562efde84d36fc50e7957e920499
Nek (Maxime Veber) <nek.dev@gmail.com>
2015-01-10 23:52:01 +0100

    Removed useless method

--- src/App/NekoWiki.php
+++ src/App/NekoWiki.php
@@ -2,13 +2,8 @@
 
 namespace App;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NekoWiki extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-    }
 }
