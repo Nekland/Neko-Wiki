a8b79c43a16017065405a0cba93defc32d06b2d5
Nek (Maxime Veber) <nek.dev@gmail.com>
2015-12-22 23:41:09 +0100

    Added get item on elasticsearch

--- src/App/Search/ElasticSearch/ElasticaTransformer.php
+++ src/App/Search/ElasticSearch/ElasticaTransformer.php
@@ -4,6 +4,7 @@ namespace App\Search\ElasticSearch;
 
 
 use App\Entity\PageTranslation;
+use Doctrine\ORM\EntityManager;
 use Elastica\Client;
 use Elastica\Document;
 use Elastica\Query;
@@ -14,6 +15,7 @@ class ElasticaTransformer
     private $index;
     private $client;
     private $serializer;
+    private $entityManager;
 
     /**
      * ElasticaTransformer constructor.
@@ -21,22 +23,57 @@ class ElasticaTransformer
      * @param Client     $elasticaClient
      * @param Serializer $serializer
      */
-    public function __construct($index, Client $elasticaClient, Serializer $serializer)
+    public function __construct($index, Client $elasticaClient, Serializer $serializer, EntityManager $entityManager)
     {
         $this->client = $elasticaClient;
         $this->index = $elasticaClient->getIndex($index);
         $this->serializer = $serializer;
+        $this->entityManager = $entityManager;
     }
 
-    public function get($query)
+    public function get($query, $entity='NekoWiki:PageTranslation')
     {
+        if (!$entity instanceof PageTranslation) {
+            throw new \Exception(sprintf('Entity %s currently not supported in elasticsearch persist.', $entity));
+        }
 
+
+        $type = $this->index->getType('page');
+
+        $search = new \Elastica\Search($this->client);
+        $search->addIndex($this->index);
+        $search->addType($type);
+
+        $search->setQuery($query);
+
+        $results = $search->search();
+        if ($results->count() === 0) {
+            return [];
+        }
+
+        $ids = [];
+
+        foreach ($results as $result) {
+            $ids[] = $result->getId();
+        }
+
+        $items = $this->entityManager->getRepository('NekoWiki:PageTranslation')->findByIds($ids);
+
+        $idPos = array_flip($ids);
+        usort($items, function ($a, $b) use ($idPos) {
+            return $idPos[$a->getId()] > $idPos[$b->getId()];
+        });
+
+        // Idea : we could add a new interface HighlightableModelInterface
+        // they do it in FOSElasticaBundle
+
+        return $items;
     }
 
     public function save($entity)
     {
         if (!$entity instanceof PageTranslation) {
-            throw new \Exception(sprintf('Class %s currently not supported in elasticsearch persist.', get_class($entity)));
+            throw new \Exception(sprintf('Entity %s currently not supported in elasticsearch persist.', get_class($entity)));
         }
 
         $type = $this->index->getType('page');
@@ -49,13 +86,23 @@ class ElasticaTransformer
 
         $result = $search->search();
         if ($result->count() > 0) {
-            $type->deleteDocument($entity->getId());
+            $this->delete($entity);
         }
-        $type->getIndex()->refresh();
 
         $document = new Document($entity->getId(), $this->serializer->serialize($entity, 'json'));
         $type->addDocument($document);
         $type->getIndex()->refresh();
 
+    }
+
+    public function delete($entity)
+    {
+        if (!$entity instanceof PageTranslation) {
+            throw new \Exception(sprintf('Entity %s currently not supported in elasticsearch persist.', get_class($entity)));
+        }
+
+        $type = $this->index->getType('page');
+        $type->deleteDocument($entity->getId());
+        $type->getIndex()->refresh();
     }
 }
